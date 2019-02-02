683d1bff43f5900596364bee85334668cc72bbb9
AndreiCalazans <andreixoc@hotmail.com>
2019-02-02 17:56:17 -0200

    init

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <div className='nav-bar'>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </div>
+          <div className="App">
+            <h1 style={{ margin: 30 }}>Fib calculator version 2</h1>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
